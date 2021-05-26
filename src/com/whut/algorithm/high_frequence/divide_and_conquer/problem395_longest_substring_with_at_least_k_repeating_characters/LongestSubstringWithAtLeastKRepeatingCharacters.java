967315048e6afb3028f2eb992839bacff493c5f8
woshilaladui <782799981@qq.com>
2021-05-26 12:54:02 +0800

    分治法和常规题目
    leetcode  395. 至少有 K 个重复字符的最长子串
    
    leetcode 973 closest_points_to_origin

---
+++ src/com/whut/algorithm/high_frequence/divide_and_conquer/problem395_longest_substring_with_at_least_k_repeating_characters/LongestSubstringWithAtLeastKRepeatingCharacters.java
@@ -0,0 +1,82 @@
+package com.whut.algorithm.high_frequence.divide_and_conquer.problem395_longest_substring_with_at_least_k_repeating_characters;
+
+import com.whut.algorithm.dynamicPlaning.test.M;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Administrator
+ * @version 1.0.0
+ * @date 2021/5/26 12:14
+ * @desription
+ */
+public class LongestSubstringWithAtLeastKRepeatingCharacters {
+
+    public int longestSubstring(String s, int k) {
+        return longestSubstring(s, k, 0, s.length() - 1);
+    }
+
+    public int longestSubstring(String s, int k, int low, int high) {
+
+        char[] arrayS = s.toCharArray();
+
+        /**
+         * 终止条件
+         *
+         * 区间长度小于k，不存在结果
+         */
+        if (high - low + 1 < k)
+            return 0;
+
+        //统计区间的字符出现频次
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = low; i <= high; i++) {
+            char ch = arrayS[i];
+            if (map.containsKey(ch))
+                map.put(ch, map.get(ch) + 1);
+            else
+                map.put(ch, 1);
+        }
+
+
+        //在区间长度>=k的前提下，如果low位置上的字符出现的次数小于k
+        while (high - low + 1 >= k && map.get(arrayS[low]) < k)
+            low++;
+
+        //在区间长度>=k的前提下，如果low位置上的字符出现的次数小于k
+        //int temp = map.get(arrayS[high]);
+        //System.out.println(temp);
+        while (high - low + 1 >= k && map.get(arrayS[high]) < k)
+            high--;
+
+        //再次判断区间长度是否满足要求，因为low,high发生了改变
+        if (high - low + 1 < k)
+            return 0;
+
+        //遍历当前区间
+        for (int i = low; i <= high; i++) {
+            // 如果在区间内的出现次数 < k，递归考察两侧的区间
+            if (map.get(arrayS[i]) < k) {
+                return Math.max(
+                        longestSubstring(s, k, low, i - 1),
+                        longestSubstring(s, k, i + 1, high)
+                );
+            }
+        }
+        // 当前区间的字符出现次数都 >= k，当前区间满足要求，返回该长度
+        return high - low + 1;
+
+    }
+
+    public static void main(String[] args) {
+        LongestSubstringWithAtLeastKRepeatingCharacters longestSubstringWithAtLeastKRepeatingCharacters = new LongestSubstringWithAtLeastKRepeatingCharacters();
+
+        /**
+         * "aabcabb"
+         * 3
+         */
+        System.out.println(longestSubstringWithAtLeastKRepeatingCharacters.longestSubstring("aabcabb", 3));
+    }
+}
--- src/com/whut/algorithm/proxy/Test.java
+++ src/com/whut/algorithm/proxy/Test.java
@@ -4,6 +4,8 @@ import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -12,13 +14,50 @@ import java.util.*;
  * @date 2021/2/22 10:46
  * @desription
  */
-public class Test implements CallbackHandler{
-    public static void main(String[] args) {
+public class Test implements CallbackHandler {
+    public static void main(String[] args) throws ParseException {
 
-        Set<String> set = new HashSet<>();
+        Integer a = 128;
+        Integer b = 128;
 
-        set.add("apple");
-     //   javax.security.auth.callback.Callback
+        System.out.println(a == b);
+
+
+
+//        String startStr = "2021-04-20 10:00:00";
+//        String endStr = "2021-04-20 10:00:49";
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        Date start = sdf.parse(startStr);
+//        Date end = sdf.parse(endStr);
+//
+//        System.out.println((start.getTime() - end.getTime()) / (1000));
+//
+//
+//        Map<Integer, Integer[]> map = new TreeMap<>(new Comparator<Integer>() {
+//            @Override
+//            public int compare(Integer o1, Integer o2) {
+//                return o1 - o2;
+//            }
+//        });
+//
+//        map.put(10, new Integer[]{1, 3});
+//        map.put(8, new Integer[]{-2, 2});
+//
+//        for (Map.Entry entry : map.entrySet()) {
+//            System.out.println(entry.getKey());
+//
+//            Integer[] array;
+//
+//            array = (Integer[]) entry.getValue();
+//
+//            System.out.println(array[0] + " " + array[1]);
+//        }
+
+
+//        Set<String> set = new HashSet<>();
+//
+//        set.add("apple");
+//     //   javax.security.auth.callback.Callback
 
 
     }
@@ -27,4 +66,6 @@ public class Test implements CallbackHandler{
     public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
 
     }
+
+
 }
